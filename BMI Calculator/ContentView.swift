7155649df8f822ca5c51808ac6d18020c56e1e0c
Stanley Sim <simyseng@gmail.com>
2026-02-18 11:38:29 +0800

    Update ContentView.swift

--- BMI Calculator/ContentView.swift
+++ BMI Calculator/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var weight = ""
     @State private var height = ""
-    @FocusState private var isEditing: Bool
+    @FocusState private var isFocused: Bool = false
     
     var body: some View {
         VStack {
@@ -21,14 +21,13 @@ struct ContentView: View {
                             .font(.headline)
                         TextField("kilogram", text: $weight)
                             .multilineTextAlignment(.trailing)
-                            .focused($isEditing)
+                            .focused(isFocused)
                     }
                     HStack {
                         Text("Height")
                             .font(.headline)
                         TextField("metre", text: $height)
                             .multilineTextAlignment(.trailing)
-                            .focused($isEditing)
                     }
                 }
                 
@@ -36,47 +35,27 @@ struct ContentView: View {
                     HStack {
                         Text("BMI")
                             .font(.headline)
-                        if let w = Double(weight), let h = Double(height){
-                            Text(String(calculateBMI(weight: w, height: h)))
+                        Spacer() // Text view does not have a multilineTextAlignment modifier like TextField.
+                        if let w = Double(weight), let h = Double(height){ // Cast String to Double if TextFields have text.
+                            Text(String(format:"%.1f", calculateBMI(weight: w, height: h)))
+                                .multilineTextAlignment(.trailing)
 
                         } else {
-                            Text("")
+                            Text("No value").foregroundStyle(.tertiary)
                         }
-                    }
-                    
-                }
-                
-                Section("Description") {
-                    if let w = Double(weight), let h = Double(height){
-                        let bmi = calculateBMI(weight: w, height: h)
-                        Text(String(bmiDescription(bmi)))
-                    } else {
-                        Text("")
                     }
                 }
             }
         }
     }
 }
-
+/*
+ * Create a function that takes in two arguments weight and height.
+ * Function returns a Double
+ */
 func calculateBMI(weight: Double, height: Double) -> Double {
     let bmi = weight / pow(height, 2)
     return bmi
-}
-
-func bmiDescription(_ bmi: Double) -> String {
-    switch bmi {
-    case 0 ..< 18.5:
-        return "Possible nutritional deficiency and osteoporosis."
-    case 18.5 ..< 22.9:
-        return "Low risk (healthy range)."
-    case 22.9 ..< 27.4:
-        return "Moderate risk of developing heart disease, high blood pressure, stroke, diabetes mellitus"
-    case 27.4 ..< 40:
-        return "High risk of developing heart disease, high blood pressure, stroke, diabetes mellitus. Metabolic Syndrome."
-    default:
-        return ""
-    }
 }
 
 #Preview {
